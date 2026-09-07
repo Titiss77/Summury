0bfb9961deb95da3b2abd434f1df16ee883e08d6
Titiss77 <Mathis.fcs.lav@gmail.com>
2026-09-07 21:26:12 +0200

    Update Session.php

--- htdocs/app/Config/Session.php
+++ htdocs/app/Config/Session.php
@@ -42,7 +42,11 @@ class Session extends BaseConfig
      * The number of SECONDS you want the session to last.
      * Setting to 0 (zero) means expire when the browser is closed.
      */
-    public int $expiration = 7200;
+    /**
+     * @var int
+     * Expire après 24 heures (86400 secondes)
+     */
+    public int $expiration = 86400;
 
     /**
      * --------------------------------------------------------------------------
