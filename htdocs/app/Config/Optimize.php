831659173cdc73ce503d8ecf36f277f7c11fb449
Titiss77 <Mathis.fcs.lav@gmail.com>
2026-09-22 23:37:41 +0200

    Update Optimize.php

--- htdocs/app/Config/Optimize.php
+++ htdocs/app/Config/Optimize.php
@@ -4,29 +4,15 @@ declare(strict_types=1);
 
 namespace Config;
 
-/**
- * Optimization Configuration.
- *
- * NOTE: This class does not extend BaseConfig for performance reasons.
- *       So you cannot replace the property values with Environment Variables.
- */
 class Optimize
 {
     /**
-     * --------------------------------------------------------------------------
-     * Config Caching
-     * --------------------------------------------------------------------------.
-     *
      * @see https://codeigniter.com/user_guide/concepts/factories.html#config-caching
      */
-    public bool $configCacheEnabled = true;
+    public bool $configCacheEnabled = false; // <-- Passer à false
 
     /**
-     * --------------------------------------------------------------------------
-     * Config Caching
-     * --------------------------------------------------------------------------.
-     *
      * @see https://codeigniter.com/user_guide/concepts/autoloader.html#file-locator-caching
      */
-    public bool $locatorCacheEnabled = true;
+    public bool $locatorCacheEnabled = false; // <-- Passer à false
 }
