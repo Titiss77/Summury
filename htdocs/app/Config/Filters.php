32de9c7c120c6494c7dbe81d22e83021646df4b8
Titiss77 <Mathis.fcs.lav@gmail.com>
2026-09-22 18:18:51 +0200

    jsp

--- htdocs/app/Config/Filters.php
+++ htdocs/app/Config/Filters.php
@@ -38,7 +38,7 @@ class Filters extends BaseFilters
 
     public array $required = [
         'before' => [
-            'forcehttps', // Géré via .htaccess maintenant
+            //'forcehttps',
             'pagecache',
         ],
         'after' => [
